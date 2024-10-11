94c5488af39f56fc66671cb5c0812ee2d2e9d7e8
el-python-2024 <el.freelancer@gmail.com>
2024-10-11 23:12:44 +0300

    функция print

--- main07102024.py
+++ main07102024.py
@@ -1,14 +1,10 @@
-print("hello world")
-a = 5
-print(a)
-x = -10
-y = abs(x) + 5
-print(y)
-z = x + y
-print(z)
-b = z**2
-print(b)
-print(x)
+print("Hello,", end=" ")
+print("world!")
+
+
+
+
+
 
 
 
